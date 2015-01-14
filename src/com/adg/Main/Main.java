e49db707825f252e988430f9f319d7beadd847f8
ag8 <andrew2000g@gmail.com>
2015-01-14 17:54:05 -0500

    Changed to Object-Oriented
    Note that I use my API Key

--- src/com/adg/Main/Main.java
+++ src/com/adg/Main/Main.java
@@ -1,36 +1,35 @@
 package com.adg.Main;
 
-import android.app.Activity;
-import android.app.FragmentTransaction;
+import android.app.*;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
-//import android.support.v4.app.FragmentManager;
-import android.view.KeyEvent;
-import android.view.MotionEvent;
-import android.view.View;
+import android.support.v4.app.NotificationCompat;
 import android.view.Window;
-import android.widget.FrameLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
-import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.model.*;
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Tim
- * Date: 05.10.13
- * Time: 10:12
- */
-public class Main extends Activity {
+public class Main extends Activity implements GoogleMap.OnMarkerClickListener, GoogleMap.OnInfoWindowClickListener {
+    public GoogleMap mMap;
+    public ArrayList<Person> people = new ArrayList<Person>();
+    Marker user;
+    Marker dyushka;
+    Marker grishka;
+    Marker alik;
+    Marker natasha;
+    Marker google;
+    ArrayList<String> sickMarkers = new ArrayList<String>();
     private MapFragment mMapFragment;
     private ProgressBar progressBar;
     //private ViewButton toListButton;
@@ -38,7 +37,6 @@ public class Main extends Activity {
     private double userLng;
     private double userLat;
     private boolean touchDown;
-    public GoogleMap mMap;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -53,13 +51,49 @@ public class Main extends Activity {
 
         //Get last known user location.
         //TODO: Get live-updating user location from listener. Use FINE_LOCATION
-        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
         lat = location.getLatitude();
         lng = location.getLongitude();
 
+
         userLat = lat;
         userLng = lng;
+
+
+
+
+        //Create people
+        createPeople();
+
+
+
+
+        //TODO: Create a notification method!
+        //Create a notification with the latitude and longitude
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.numbat)
+                        .setContentTitle("Coordinates")
+                        .setContentText("Reporting Data from " + userLat + ", " + userLng);
+        Intent resultIntent = new Intent(this, Main.class);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(Main.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(3, mBuilder.build());
+
+
+
+
 
         zoom = 18.0f;
         CameraPosition cameraPosition = new CameraPosition(new LatLng(lat, lng), zoom, 0, 0);
@@ -76,18 +110,15 @@ public class Main extends Activity {
                 getFragmentManager().beginTransaction();
         fragmentTransaction.add(R.id.mapView, mMapFragment);
         fragmentTransaction.commit();
-        executePendingTransactions();
         MapsInitializer.initialize(this);
 
-        //Wait after commit
-        try {
-            Thread.sleep(10);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
+        //TODO: Fix executePendingTransactions()! It does not work!
+        android.app.FragmentManager f = getFragmentManager();
+        f.executePendingTransactions();
 
         //Set the User's coordinates into the TextView bar above the map
-        TextView notices = (TextView)findViewById(R.id.notices);
+        TextView notices = (TextView) findViewById(R.id.notices);
 
         notices.setText("You are currently reporting coordinates from " + lat + ", " + lng);
 
@@ -95,56 +126,81 @@ public class Main extends Activity {
 
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
-            Toast.makeText(getApplicationContext(), "Cannot access Google Map. Due to dynamic fragments, try using executePendingTransactions()", Toast.LENGTH_LONG).show();
             //Retry
             mMap = mMapFragment.getMap();
-            Toast.makeText(getApplicationContext(), "Cannot access Google Map.", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Cannot access Google Map.", Toast.LENGTH_SHORT).show();
             if (mMap != null) {
                 // The Map is verified. It is now safe to manipulate the map.
                 Toast.makeText(getApplicationContext(), "Google Map Accessed Successfully.", Toast.LENGTH_LONG).show();
             }
         }
-
-//        if (mMap != null) {
-//            mMap.addMarker(new MarkerOptions()
-//                    .position(new LatLng(lat, lng))
-//                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
-//                    .title("You"));
-//        }
     }
 
-    public void executePendingTransactions() {
-//        boolean d = FragmentManager.executePendingTransactions();
+    public void createPeople() {
+        people.add(new Person(0, "You", userLat, userLng));
+        people.add(new Person(1, "Dyushka", 42.424974, -71.2140763));
+        people.add(new Person(2, "Grishka", 42.305753, -71.242723));
+        people.add(new Person(3, "Natasha", 42.3233006, -71.2041064));
+        people.add(new Person(4, "Alik", 42.2940449, -71.377857));
+        people.add(new Person(5, "Android", 37.4219184, -122.0839834));
     }
 
     public void onUserLeaveHint() {
         mMap = mMapFragment.getMap();
         if (mMap == null) {
-            Toast.makeText(getApplicationContext(), "Cannot access Google Map.", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Cannot access Google Map. Ever.", Toast.LENGTH_LONG).show();
         } else {
             Toast.makeText(getApplicationContext(), "Accessed Google Map. Please Re-open Numbat to continue.", Toast.LENGTH_LONG).show();
-            mMap.addMarker(new MarkerOptions()
-                    .position(new LatLng(userLat, userLng))
-                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
-                    .title("You"));
-            mMap.addMarker(new MarkerOptions()
-                    .position(new LatLng(42.424974, -71.2140763))
-                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
-                    .title("Dyushka"));
-            mMap.addMarker(new MarkerOptions()
-                    .position(new LatLng(42.305753, -71.242723))
-                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
-                    .title("Grishka"));
-            mMap.addMarker(new MarkerOptions()
-                    .position(new LatLng(42.3233006, -71.2041064))
-                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
-                    .title("Natasha"));
-            mMap.addMarker(new MarkerOptions()
-                    .position(new LatLng(42.2940449, -71.377857))
-                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW))
-                    .title("Alik"));
+            MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.ding);
+            mediaPlayer.start(); // no need to call prepare(); create() does that for you
+            makeMarkers();
         }
     }
+
+    public void makeMarkers() {
+        mMap.setOnInfoWindowClickListener(this);
+        mMap.setOnMarkerClickListener(this);
+
+        //Create the markers
+        for (Person person : people) {
+            mMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(person.getLat(), person.getLng()))
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+                    .title(person.getName())
+                    .snippet("Healthy =)"));
+        }
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        if (!sickMarkers.contains(marker.getTitle())) {
+            mMap.addMarker(new MarkerOptions()
+                    .position(marker.getPosition())
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+                    .title(marker.getTitle())
+                    .snippet("Sick!"));
+            sickMarkers.add(marker.getTitle());
+
+        } else {
+            mMap.addMarker(new MarkerOptions()
+                    .position(marker.getPosition())
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+                    .title(marker.getTitle())
+                    .snippet("Healthy =)"));
+            sickMarkers.remove(marker.getTitle());
+        }
+        return false;
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+        new AlertDialog.Builder(this)
+                .setTitle("Information")
+                .setMessage(marker.getTitle() + " is currently " + marker.getSnippet())
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+
 
 //    public void onPause() {
 //
---
+++ src/com/adg/Main/Person.java
@@ -0,0 +1,51 @@
+package com.adg.Main;
+
+import com.google.android.gms.maps.model.Marker;
+
+public class Person {
+    private int id;
+    private String name;
+    private double lat;
+    private double lng;
+
+    private boolean healthy;
+
+    public Marker marker;
+
+    public Person(int id, String name, double lat, double lng) {
+        this.id = id;
+        this.name = name;
+        this.lat = lat;
+        this.lng = lng;
+
+        this.healthy = true; //By default
+    }
+
+    //Getters
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public double getLng() {
+        return lng;
+    }
+
+    public boolean isHealthy() {
+        return healthy;
+    }
+
+    //Setters
+
+    public void setHealthy(boolean healthy) {
+        this.healthy = healthy;
+    }
+}
